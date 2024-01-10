a7a50aba1ab9c411067ba8c9e7ef02c3b34ca095
Dasun Tharanga <hello@dasun.dev>
2024-01-10 19:21:49 +0530

    feat: Create LivewireQuillTextEditorServiceProvider.php

---
+++ src/LivewireQuillTextEditorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dasundev\LivewireQuillTextEditor;
+
+use Dasundev\LivewireQuillTextEditor\Http\Livewire\QuillTextEditor;
+use Livewire\Livewire;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LivewireQuillTextEditorServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('livewire-quill-text-editor')
+            ->hasViews();
+    }
+
+    public function bootingPackage(): void
+    {
+        $this->registerLivewireComponent();
+    }
+
+    public function registerLivewireComponent(): void
+    {
+        Livewire::component('quill-text-editor', QuillTextEditor::class);
+    }
+}
