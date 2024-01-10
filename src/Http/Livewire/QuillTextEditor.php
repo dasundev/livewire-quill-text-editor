a03f1d08083674bf502ff464e9595f78eac735fb
Dasun Tharanga <hello@dasun.dev>
2024-01-10 19:31:08 +0530

    feat: Add render method

--- src/Http/Livewire/QuillTextEditor.php
+++ src/Http/Livewire/QuillTextEditor.php
@@ -2,8 +2,13 @@
 
 namespace Dasundev\LivewireQuillTextEditor\Http\Livewire;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class QuillTextEditor extends Component
 {
+    public function render(): View
+    {
+        return view('livewire-quill-text-editor::livewire.quill-text-editor');
+    }
 }
