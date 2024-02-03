24211379fa86f773d47dbbfb7aa1ba83d3ec95bc
Patrick <patrickvanderpols@live.nl>
2024-02-03 03:02:11 +0100

    fix: Set the wire:model value on first load

--- resources/views/livewire/quill-text-editor.blade.php
+++ resources/views/livewire/quill-text-editor.blade.php
@@ -7,7 +7,7 @@
     const quill = new Quill('#' + @js($quillId), {
         theme: @js($theme)
     });
-
+    quill.root.innerHTML = $wire.get('value');
     quill.on('text-change', function () {
         let value = document.getElementsByClassName('ql-editor')[0].innerHTML;
         @this.set('value', value)
