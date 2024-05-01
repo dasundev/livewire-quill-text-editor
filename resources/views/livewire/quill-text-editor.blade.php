bbc6c52c291d3fe2b4afe18dfaa3968460b8eb27
muhammedjafer <mohammadjafer990@gmail.com>
2024-05-01 10:23:28 +0300

    fixed constant request to server as the user types into the editor, aso added toolbar option for snow theme

--- resources/views/livewire/quill-text-editor.blade.php
+++ resources/views/livewire/quill-text-editor.blade.php
@@ -4,15 +4,54 @@
 
 @script
 <script>
+    let toolbarOptions = [
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'font': [] }],
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+
+        ['image', 'video'],
+        ['clean']                                         // remove formatting button
+    ];
+
     const quill = new Quill('#' + @js($quillId), {
+        modules: {
+            toolbar: toolbarOptions
+        },
         theme: @js($theme)
     });
     
     quill.root.innerHTML = $wire.get('value');
     
-    quill.on('text-change', function () {
-        let value = quill.root.innerHTML;
-        @this.set('value', value);
+    let timeout = null;
+
+    function handleTextChange() {
+        if (timeout !== null) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(function() {
+            let value = quill.root.innerHTML;
+            @this.set('value', value);
+        }, 1000);
+    }
+
+    quill.on('text-change', function() {
+        handleTextChange();
+    });
+
+    quill.on('selection-change', function() {
+        if (quill.hasFocus()) {
+            handleTextChange();
+        }
     });
 </script>
 @endscript
