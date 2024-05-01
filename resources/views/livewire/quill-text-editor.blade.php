94e05a0b514ee97e76be827e23a166fc80a5a86a
Muhammed <56185049+muhammedjafer@users.noreply.github.com>
2024-05-01 11:42:38 +0300

    removed the toolbar option for the editor

--- resources/views/livewire/quill-text-editor.blade.php
+++ resources/views/livewire/quill-text-editor.blade.php
@@ -4,28 +4,7 @@
 
 @script
 <script>
-    let toolbarOptions = [
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-        [{ 'font': [] }],
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'align': [] }],
-
-        ['image', 'video'],
-        ['clean']                                         // remove formatting button
-    ];
-
     const quill = new Quill('#' + @js($quillId), {
-        modules: {
-            toolbar: toolbarOptions
-        },
         theme: @js($theme)
     });
     
